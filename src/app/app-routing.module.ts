f028d8cd76b338cfc8e547b7b5dc02abd8b149fd
PauloGustavoLatta <paulogn.lata@gmail.com>
2023-08-24 18:32:03 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Início',
+    },
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
+    data: {
+      title: 'Sobre mim',
+    },
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+    data: {
+      title: 'Portfolio',
+    },
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    data: {
+      title: 'Projetos',
+    },
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: {
+      title: 'Contato',
+    },
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
