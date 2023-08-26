221fadd934c5ed9f2b540a5cd1c34b57492d880c
PauloGustavoLatta <paulogn.lata@gmail.com>
2023-08-26 12:39:14 -0300

    fix issue_8678pu0xk add about me page

--- src/app/components/area-about/area-about.component.ts
+++ src/app/components/area-about/area-about.component.ts
@@ -3,10 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-area-about',
   templateUrl: './area-about.component.html',
-  styleUrls: ['./area-about.component.scss']
+  styleUrls: ['./area-about.component.scss'],
 })
 export class AreaAboutComponent {
-  @Input() iconName: string = '';
   @Input() icon: string = '';
   @Input() title: string = '';
 }
--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent {
+  public items = [
+    'Graduação em Ciência da Computação pelo Instituto de Ensino Superior da Grande Florianópolis (IESGF - UNIP);',
+    'Desenvolvedor Front-End à 4 anos;',
+    'Experiência com os frameworks Angular e React;',
+    'Estudando Java Spring Boot com o intuito de ser Full Stack, e inglês por entender ser essencial para um desenvolvedor;',
+  ];
 
+  constructor() {}
+
+  curriculo(): void {
+    window.open(
+      'https://drive.google.com/file/d/1s9I4R_VazzyVfwgVvOrU5Cf4eOYHJGkX/view?usp=drive_link',
+      '_blank'
+    );
+  }
 }
