008c192df72821b511a563626cd49b81a63eb94b
paulolatta <paulogn.lata@gmail.com>
2023-08-26 18:20:08 -0300

    initial commit

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-
+  public items = [
+    { title: 'AngularTS', subtitle: 'Frameworks Front-End TypeScript' },
+    { title: 'TypeScript', subtitle: 'Superconjunto de JavaScript' },
+    { title: 'Angular Material', subtitle: 'Componente Design para Angular' },
+    {
+      title: 'Bootstrap',
+      subtitle: 'Kit de ferramentas de CSS para front-end',
+    },
+    { title: 'ChartJS', subtitle: 'Biblioteca de gráficos' },
+  ];
 }
