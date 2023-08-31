23caa6431798591f2a3ffa2cf6c8f7cb969d7c54
paulolatta <paulogn.lata@gmail.com>
2023-08-31 14:33:09 -0300

    fix issue_8678qucyv add emailjs

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,17 +1,24 @@
 import {
   FormsModule,
-  NgForm,
   ReactiveFormsModule,
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
 } from '@angular/forms';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarModule,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-contact',
@@ -25,11 +32,18 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
 })
 export class ContactComponent {
   public icon: string = 'contact_mail';
   public title: string = 'Contato';
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   form: UntypedFormGroup = this.formBuilder.group({
     name: [null, Validators.required],
@@ -37,13 +51,41 @@ export class ContactComponent {
     message: [null, Validators.required],
   });
 
-  constructor(private formBuilder: UntypedFormBuilder) {}
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private _snackBar: MatSnackBar
+  ) {}
 
-  enviar(): void {
+  public sendEmail(e: Event) {
     const values = {
       name: this.form.get('name')?.value,
       email: this.form.get('email')?.value,
       message: this.form.get('message')?.value,
     };
+
+    emailjs
+      .send('gmailMessage', 'template_4g2kdto', values, '7qePGAIR7YEafwa0H')
+      .then(() => {
+        this._snackBar.open(
+          'E-mail enviado com sucesso, entrarei em contato o mais breve possível!!',
+          'Fechar',
+          {
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            duration: 5000,
+          }
+        );
+      })
+      .catch(() => {
+        this._snackBar.open(
+          'Ocorreu um erro na hora de enviar seus dados, tente novamente.',
+          'Fechar',
+          {
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            duration: 5000,
+          }
+        );
+      });
   }
 }
